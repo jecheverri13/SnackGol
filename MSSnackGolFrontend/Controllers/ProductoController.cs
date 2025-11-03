09eb59fccf290c49f97e9c9ec34dcb63daa96d17
Junior <jecheverri@consensussa.com>
2025-11-03 15:57:51 -0500

    feat: visualizar_producto

---
+++ MSSnackGolFrontend/Controllers/ProductoController.cs
@@ -0,0 +1,53 @@
+﻿using LibraryConnection.Dtos;
+using LibraryEntities.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Net.Http;
+
+namespace MSSnackGolFrontend.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductoController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private string GetOrCreateSessionToken()
+        {
+            const string key = "SessionToken";
+            if (!HttpContext.Session.TryGetValue(key, out _))
+            {
+                var token = Guid.NewGuid().ToString("N");
+                HttpContext.Session.SetString(key, token);
+            }
+            return HttpContext.Session.GetString(key)!;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var token = GetOrCreateSessionToken();
+            var client = _httpClientFactory.CreateClient("Api");
+            client.DefaultRequestHeaders.Remove("X-Session-Token");
+            client.DefaultRequestHeaders.Add("X-Session-Token", token);
+
+            var vm = new Models.CartPageViewModel();
+            try
+            {
+                var productsResp = await client.GetFromJsonAsync<Response<List<ProductDto>>>("api/ProductManagment/List");
+                vm.Products = productsResp?.response ?? new List<ProductDto>();
+            }
+            catch
+            {
+                // Fallback: catálogo vacío si API no responde
+                vm.Products = new List<ProductDto>();
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ MSSnackGolFrontend/Views/Producto/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MSSnackGolFrontend.Views.Carrito
+{
+    public class ProductoModel : PageModel
+    {
+        private readonly ILogger<ProductoModel> _logger;
+
+        public ProductoModel(ILogger<ProductoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Producto page accessed");
+        }
+    }
+}
